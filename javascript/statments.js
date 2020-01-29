a857f49edd7352fb0ba0f37cbb0b894c3edf773d
Joshua Epstein <jdepst16@stlawu.edu>
2020-01-29 13:37:07 -0500

    JavaScropt syntax

---
+++ javascript/statments.js
@@ -0,0 +1,26 @@
+//Statments synax in JavaScript
+
+//Constants
+const pi = 3.14;
+
+//Printing
+console.log(pi);
+
+//Loops for
+for (let x = 0; x < 3; x++){
+  console.log(x);}
+
+//Loops while
+
+let x = 0
+while(x < 3) {
+  console.log(x);
+  x += 1; }
+
+//if
+if (x === 3){
+  console.log('x is now 3');}
+
+else {
+  console.log(`x is now ${x}`);
+}
