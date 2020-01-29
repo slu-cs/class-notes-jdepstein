9898eac53ff3338b7d6857e56d782dc5e86f16c1
Joshua Epstein <jdepst16@stlawu.edu>
2020-01-29 14:05:02 -0500

    practice JavaScript

---
+++ javascript/Practice.js
@@ -0,0 +1,38 @@
+
+
+//Sum of square 1-100
+console.log('Sum Of Squares');
+for (let x = 1; x < 11; x++){
+  console.log(x**2);}
+
+
+console.log('Fib');
+let x = 0
+let y = 1
+while (y < 1000) {
+  let z = y
+  y = x + y
+  x = z}
+
+console.log('Div 3 5');
+for (let c = 1; c < 101; c++){
+  if ( ((c % 5) === 0 or (c % 3) === 0) and (!((c % 5) === 0 and (c % 3) === 0))) {
+    console.log(c);
+  }
+}
+
+console.log(smallest div);
+let t = True
+let w = 0
+while t{ 
+  w += 1
+  let count = 0
+  for (let q = 1; x < 11; x++){
+
+    if (w % q === 0){
+      count += 1}
+
+    if count === 10){
+      t = True}}
+}
+console.log(w);
