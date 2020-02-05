d64391e7701d722834c654b971f68a6c0a9ec07a
Joshua Epstein <jdepst16@stlawu.edu>
2020-02-05 13:25:40 -0500

    javascript call backs

---
+++ javascript/callback.js
@@ -0,0 +1,23 @@
+//Snytax for callbacks
+
+//This function uses another function
+//to transfrom an array
+
+const map = function(array, transfrom){
+  const copy = [];
+
+  for (const element of array) {
+    copy.push(transfrom(element));
+  }
+
+  return copy;
+};
+
+//This function can be used a sthe transfrom
+const square = function(x){
+  return x ** 2;
+};
+
+const numbers = [1, 2, 3, 4, 5];
+
+map(numbers, square);
