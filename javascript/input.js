c9c337d1eb17494dede1eece4e3a78df07b1975f
Joshua Epstein <jdepst16@stlawu.edu>
2020-02-05 13:44:45 -0500

    Javascript console input

---
+++ javascript/input.js
@@ -0,0 +1,14 @@
+//Syntax fro console and file inputS
+
+const readline = require('readline');
+
+//Console configuration
+
+const user= readline.createInterface({
+  input: process.stdin,
+  output:process.stdout
+})
+
+user.question('Enter a filename: ', function(filename){
+    console.log(filename);
+});
