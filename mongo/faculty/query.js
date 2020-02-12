933af16ce41d3190ec31e288024fc996f8e363cc
Joshua Epstein <jdepst16@stlawu.edu>
2020-02-12 13:16:42 -0500

    more queries

--- mongo/faculty/query.js
+++ mongo/faculty/query.js
@@ -7,9 +7,27 @@ const Professor = require('./schema');
 
 connect(); // To the database
 
-// What documents are in the collection?
+/*// What documents are in the collection?
 const query = Professor.find();
 query.exec(function(error, professors) {
   if (error) console.error(error.stack);
   console.log(professors);
-});
+});*/
+
+const queries = [
+  //What are the name sin alphabetical order?
+  Professor.find().sort('name'),
+
+  //Who started most recently
+  Professor.find().sort('-started').limit(1),
+
+  //who started in 2003
+  Professor.find().where('started').equals(2003),
+
+  //who teaches 362
+  Professor.find().where('courses').in(362),
+
+  Professor.distinct('rank')
+
+
+];
