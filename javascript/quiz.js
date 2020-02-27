19af549f3bbe054f63b214a0038b0607b56adbb1
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-27 15:57:14 -0500

    ,

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -55,7 +55,7 @@ const rep2 = function(rep){
   return console.log("I am also " + rep); }
 
 
-  user.question("What is your Name?" rep1 {
+  user.question("What is your Name?", rep1 {
 
     user.question("How are you doing?", rep2 {
 
