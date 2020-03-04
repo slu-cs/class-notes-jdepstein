9b56ecfdd1d8d59073b00ed181d5c1f77149b693
Joshua Epstein <jdepst16@stlawu.edu>
2020-03-04 13:35:24 -0500

    Express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -4,10 +4,31 @@ const express = require('express');
 // Creates the sever
 const app = express();
 
-//Respond to one reuqest
+// http://cs-linuxlab-32.stlawu.edu:3000/
 app.get('/', function(request, response){
   response.send('Hello Josh');
   });
+
+//http://cs-linuxlab-32.stlawu.edu:3000/foo
+app.get('/foo', function(request, response){
+  response.send('Hello foo');
+  });
+
+//http://cs-linuxlab-32.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response){
+  response.send('Hello bar');
+  });
+
+//http://cs-linuxlab-32.stlawu.edu:3000/zap/z     (for any z)
+app.get('/zap/:z', function(request, response){
+  response.send(`Hello ${request.params.z}`);
+  });
+
+//http://cs-linuxlab-32.stlawu.edu:3000/zap?x=&y=
+app.get('/zap', function(request, response){
+  response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
+  });
+
 
 // Starts the server
 app.listen(3000);
