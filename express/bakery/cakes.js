d3d97032c226c50016357a3e6d2a1bbff7e692fc
Joshua Epstein <jdepst16@stlawu.edu>
2020-03-09 13:48:50 -0400

    includes and expressions

--- express/bakery/cakes.js
+++ express/bakery/cakes.js
@@ -4,6 +4,12 @@ const express = require('express');
 // Create the router
 const router = express.Router();
 
+const cakes = [
+
+  {id: 'vanilla', flavor: 'Vanilla'}
+  {id: 'chocolate', flavor: 'Chocolate'}
+];
+
 // cs-linuxlab-##.stlawu.edu:3000/cakes/
 router.get('/', function(request, response) {
   response.send(`
@@ -17,12 +23,15 @@ router.get('/', function(request, response) {
 
 // cs-linuxlab-##.stlawu.edu:3000/cakes/id
 router.get('/:id', function(request, response, next) {
-  if (request.params.id === 'vanilla') {
-    response.send('Vanilla cakes');
-  } else if (request.params.id === 'chocolate') {
-    response.send('Chocolate cakes');
-  } else {
-    next(); // Pass on this request
+
+  // Pretend database lookup
+  const cake = cakes.find(cake => cake.id === request.params.id);
+
+  if (!cake){
+    next();}
+
+  else{
+    response.render('cakes/detail',{cake: cake});
   }
 });
 
